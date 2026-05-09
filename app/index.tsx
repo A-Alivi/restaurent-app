26166fb000343e31ca34f9c3406b8124cf2432ce
Alivi <hakeemalivi5@gmail.com>
2026-05-09 15:27:15 +0500

    refactor: remove SafeAreaView wrapper from index component

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
 import SSOCallbackScreen from "./sso-callback";
 
 const index = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <SSOCallbackScreen />
-      </View>
-    </SafeAreaView>
+    <View>
+      <SSOCallbackScreen />
+    </View>
   );
 };
 
