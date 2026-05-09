97cbc0aa69fdade1892b7e392480f8635300b787
Alivi <hakeemalivi5@gmail.com>
2026-05-09 15:18:27 +0500

    refactor: remove unused order logic from index component

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { orders } from "../data/orders";
 import "../global.css";
-import { useOrderStore } from "../store/useOrderStore";
 import SSOCallbackScreen from "./sso-callback";
 
 const index = () => {
-  const setOrders = useOrderStore((state) => state.setOrders);
-  useEffect(() => {
-    setOrders(orders);
-  }, []);
   return (
     <SafeAreaView>
       <View>
